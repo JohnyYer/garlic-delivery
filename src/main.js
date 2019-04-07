20722643ac40cbbdd1044e7df42eecbcd4c76034
Ivan <i.yeremenko@ext.mrdm.nl>
2019-04-08 00:10:58 +0300

    new api key

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import VueGAPI from 'vue-gapi'
 
 // create the 'options' object
 const apiConfig = {
-  apiKey: 'AIzaSyB9DcA9ehUmFlr0Qer2VzIZKYAU_tmpejk',
-  clientId: '718288481152-rpu4vgnm7mquk2813r9bf1eh613k74b7.apps.googleusercontent.com',
+  apiKey: 'AIzaSyBqWt3VvuCp-R1uPa4BBXEZXGixIo8av7s',
+  clientId: '718288481152-of86fbec0emphdv72vb3p6el9bvdkaj3.apps.googleusercontent.com',
   discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   scope: 'https://www.googleapis.com/auth/spreadsheets'
   // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
