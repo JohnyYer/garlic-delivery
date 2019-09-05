bc49daa3a86bddbf1474199e98ac238c4377615f
ivan-yeremenko <ivan.yeremenko@oracle.com>
2019-09-05 17:40:00 +0300

    added new table

--- src/components/admin/components/tables.js
+++ src/components/admin/components/tables.js
@@ -1,48 +1,8 @@
 const TABLES = [
   {
-    name: 'CallStar',
-    spreadsheetId: '1zN3l9Tr_g61G4sdE66uMyflASnEEG6bO69_KIITNpcw'
-  },
-  {
-    name: 'CallStar 19b',
-    spreadsheetId: '1_UK5QfAtHP2-1w4Dd70IBuPAKOf2IftIE_aatULxvlI'
-  },
-  {
-    name: 'Варамар',
-    spreadsheetId: '1nifWFxerRXWZFtUCeXKgF0OBXK07bnyiheGo1_IRSNw'
-  },
-  {
-    name: 'Стоматология',
-    spreadsheetId: '13KQwINnab4WeQ-ktRqLUBLCLr8YHMuz8lPJSBpFi5io'
-  },
-  {
-    name: 'DataArt',
-    spreadsheetId: '1QN1HADHAB-KuBhndAMrJijj1g6w6SV471-qJn6oPVwA'
-  },
-  {
-    name: 'ШитоКрыто',
-    spreadsheetId: '1mtLEEgx7xbkabUHfACWehXy0nYAu-1EM3KRTrAoqK44'
-  },
-  {
-    name: 'Capital',
-    spreadsheetId: '1hMm3o7qflBIfk8qqFI09ecIWDngwUAVuyqj7L_tC6FA'
-  },
-  {
-    name: 'SFM',
-    spreadsheetId: '1nvn2enDBzc1I2_1QKiejTOHroOVSUkHUInGTkHraPQU'
-  },
-  {
-    name: 'Armeyskaya',
-    spreadsheetId: '1897CojJfjMfLLTIhiDy3oXGrSlaO__-5M1UXg6RXK50'
-  },
-  {
-    name: 'Обсерваторный',
-    spreadsheetId: '1ubYj8k2zFszjxU2-6YpufI_Cjf6AOEQY-iev0e0OJn0'
-  },
-  {
-    name: 'ТПЕ Греческая',
-    spreadsheetId: '1rniL-beFEFUl6TUdXZ4JWnMMQxdW6j4fVQH_AWKpxoU'
+    name: 'Вузовский',
+    spreadsheetId: '1w2imu6RV0U21yR-Vo2S2vlJ7TiI3voOx0V5EFa0EnmY'
   }
-]
+];
 
 export default TABLES
