0e9f090331aabf95b79d04d037f1a175cd2e1b73
JohnyYer <johnyeremenko91@gmail.com>
2020-12-05 10:43:31 +0200

    Update GoogleTables.vue

--- src/components/admin/GoogleTables.vue
+++ src/components/admin/GoogleTables.vue
@@ -80,7 +80,7 @@ export default {
           gapi.client.sheets.spreadsheets.values
             .get({
               spreadsheetId: setting.spreadsheetId,
-              range: 'C3:F50'
+              range: 'C3:F150'
             })
             .then(res => {
               if (res.result.values) {
